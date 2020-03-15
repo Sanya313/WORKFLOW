77b2b5c346a2331b56aada7fab293e28028fdca8
IvanSkadorva <vaniaskadorva@gmail.com>
2020-03-15 21:05:45 +0300

    create switch part

---
+++ Bones_game.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int number = 2;
+
+
+
+	switch (number) {
+
+	case 1:
+
+		cout << "===========" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|    *    |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "===========" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "===========" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|    *    |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|    *    |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "===========" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "===========" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|  *      |" << endl;
+
+		cout << "|    *    |" << endl;
+
+		cout << "|      *  |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "===========" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "===========" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|  *   *  |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|  *   *  |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "===========" << endl;
+
+		break;
+
+	case 5:
+
+		cout << "===========" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|  *   *  |" << endl;
+
+		cout << "|    *    |" << endl;
+
+		cout << "|  *   *  |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "===========" << endl;
+
+		break;
+
+	case 6:
+
+		cout << "===========" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "|  *   *  |" << endl;
+
+		cout << "|  *   *  |" << endl;
+
+		cout << "|  *   *  |" << endl;
+
+		cout << "|         |" << endl;
+
+		cout << "===========" << endl;
+
+		break;
+
+
+
+	}
+}
